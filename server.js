122eba1a01a405615266fbfdc92a694579125636
egublog <egukei0729@gmail.com>
2022-07-29 23:30:01 +0900

    [MODIFY]auth.jsを使用する

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const PORT = 5001;
+const auth = require('./routes/auth');
+
+app.use("/auth", auth);
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
