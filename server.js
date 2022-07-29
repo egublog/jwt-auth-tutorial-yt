2937d88a0508606712a188f639301e37e5cfb79c
egublog <egukei0729@gmail.com>
2022-07-29 23:21:29 +0900

    [MODIFY]ブラウザでHello World!を確認

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
-
 const PORT = 5001;
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
 
 app.listen(PORT, () => {
   console.log("サーバーを起動中...");
