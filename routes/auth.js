06aa74cdbedb832f597b72c6c83aaa30d185a1fa
egublog <egukei0729@gmail.com>
2022-07-30 15:19:37 +0900

    [MODIFY]バリデーションチェック

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,24 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const { body, validationResult } = require("express-validator");
 
-router.get('/', (req, res) => {
-    res.send('Hello Auth!');
+router.get("/", (req, res) => {
+  res.send("Hello Auth!");
 });
 
 // ユーザー新規登録用のAPI
-router.post('/register', (req, res) => {
-    const email = req.body.email;
-    const password = req.body.password;
+router.post(
+  "/register",
+  body("email").isEmail(),
+  body("password").isLength({ min: 6 }),
+  (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
 
-    console.log(email, password);
+  console.log(email, password);
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
 });
 
 module.exports = router;
