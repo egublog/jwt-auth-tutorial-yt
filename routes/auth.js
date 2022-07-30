8cd1ee9522156cba9c09f2a2ad056b9de2464bad
egublog <egukei0729@gmail.com>
2022-07-30 15:00:23 +0900

    [ADD]ユーザーの新規登録用のAPIを作成

--- routes/auth.js
+++ routes/auth.js
@@ -4,4 +4,12 @@ router.get('/', (req, res) => {
     res.send('Hello Auth!');
 });
 
+// ユーザー新規登録用のAPI
+router.post('/register', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    console.log(email, password);
+});
+
 module.exports = router;
