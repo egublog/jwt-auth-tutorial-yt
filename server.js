55a3b09038ee6fe99d7e3b59499d8929d4a0f5b2
egublog <egukei0729@gmail.com>
2022-07-30 15:09:27 +0900

    [ADD]express.json()のコメント

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const app = express();
 const PORT = 5001;
 const auth = require('./routes/auth');
 
-app.use(express.json());
+
+app.use(express.json()); // NOTE: express.json()を使うと、req.bodyがJSON形式で受け取れる
 app.use("/auth", auth);
 
 app.get("/", (req, res) => {
