2df4606210bc3c7d80bc6a64b4f347e35d5ff0d0
egublog <egukei0729@gmail.com>
2022-08-01 02:18:36 +0900

    [CREATE]db/Post.js

---
+++ db/Post.js
@@ -0,0 +1,23 @@
+const publicPosts = [
+  {
+    title: "誰でもみれる記事",
+    body: "誰でもみれる記事です。",
+  },
+  {
+    title: "誰でもみれる記事",
+    body: "誰でもみれる記事です。",
+  },
+];
+
+const privatePosts = [
+  {
+    title: "JWTを持っているユーザーしか見れない記事",
+    body: "JWTを持っているユーザーしか見れない記事です。",
+  },
+  {
+    title: "JWTを持っているユーザーしか見れない記事",
+    body: "JWTを持っているユーザーしか見れない記事です。",
+  },
+];
+
+module.exports = { publicPosts, privatePosts };
