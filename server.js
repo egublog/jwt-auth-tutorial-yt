e208151d5c778e75e6277a4202f9c52bce89a58c
egublog <egukei0729@gmail.com>
2022-08-01 02:39:47 +0900

    [ADD]post

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const PORT = 5001;
 const auth = require('./routes/auth');
+const post = require('./routes/post');
 
 
 app.use(express.json()); // NOTE: express.json()を使うと、req.bodyがJSON形式で受け取れる
 app.use("/auth", auth);
+app.use("/post", post);
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
