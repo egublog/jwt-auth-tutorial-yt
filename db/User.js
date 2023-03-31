bf0ca5847d5a6d77c2cd1be3a07fe29549fb4f2a
egublog <egukei0729@gmail.com>
2023-03-31 14:37:46 +0900

    add: テストデータの追加

--- db/User.js
+++ db/User.js
@@ -3,6 +3,10 @@ const User = [
     email: "test@gmail.com",
     password: "test_test",
   },
+  {
+    email: "test2@gmail.com",
+    password: "test2_test2",
+  },
 ];
 
 module.exports = { User };
